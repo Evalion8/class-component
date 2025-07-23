fd4f7b4ede1909609b82148281148697f5b5f237
Evalion8 <alisultanovaeva@gmail.com>
2025-07-23 15:44:05 +0300

    refactor: class component CardList into functional

--- react-project/src/feature/CardList.tsx
+++ react-project/src/feature/CardList.tsx
@@ -1,18 +1,12 @@
 import React from 'react';
 import { Card } from './Card';
 
-interface Props {
-  items: { name: string; url: string }[];
-}
-
-export class CardList extends React.Component<Props> {
-  render() {
-    return (
-      <div className="grid grid-cols-2 md:grid-cols-3 gap-4 p-4">
-        {this.props.items.map((item) => (
-          <Card key={item.name} name={item.name} url={item.url} />
-        ))}
-      </div>
-    );
-  }
-}
+export const CardList = ({ items = [] }) => {
+  return (
+    <div className="grid grid-cols-2 md:grid-cols-3 gap-4 p-4">
+      {items.map((item) => (
+        <Card key={item.name} name={item.name} url={item.url} />
+      ))}
+    </div>
+  );
+};
