9c6dc9d5bb8d5e9d90861fe1724ba1db96c84ddd
Evalion8 <alisultanovaeva@gmail.com>
2025-07-23 19:03:28 +0300

    reafctor: change Header component into functional

--- react-project/src/feature/Header.tsx
+++ react-project/src/feature/Header.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 
-export class Header extends React.Component {
-  render() {
-    return (
-      <header className="p-4 bg-blue-600 text-white text-xl font-bold">
-        PokeSearch App (Class Components)
-      </header>
-    );
-  }
-}
+export const Header = () => {
+  return (
+    <h1 className="p-4 bg-blue-600 text-white text-xl font-bold">
+      PokeSearch App (Class Components)
+    </h1>
+  );
+};
