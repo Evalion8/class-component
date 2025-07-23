2c60b84f615e58db7573f5bccc4021f2ba79dcdb
Evalion8 <alisultanovaeva@gmail.com>
2025-07-23 15:30:57 +0300

    refactor: class component Card into functional

--- react-project/src/feature/Card.tsx
+++ react-project/src/feature/Card.tsx
@@ -1,17 +1,15 @@
 import React from 'react';
 
-interface Props {
-  name: string;
-  url: string;
-}
+// interface Props {
+//   name: string;
+//   url: string;
+// }
 
-export class Card extends React.Component<Props> {
-  render() {
-    return (
-      <div className="border p-4 rounded shadow">
-        <h3 className="text-lg font-semibold">{this.props.name}</h3>
-        <p className="text-sm text-gray-500">{this.props.url}</p>
-      </div>
-    );
-  }
-}
+export const Card = () => {
+  return (
+    <div className="border p-4 rounded shadow">
+      <h3 className="text-lg font-semibold"></h3>
+      <p className="text-sm text-gray-500"></p>
+    </div>
+  );
+};
